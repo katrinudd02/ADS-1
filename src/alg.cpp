87732007be61bef661c99b5be3e7e65b609a7bdc
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-02-27 23:00:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,69 +12,53 @@ bool checkPrime(uint64_t value) {
         flag = true;
     }
     return flag;
-
-    //if (value == 1) {
-    //    return false;
-   // }
-    //
-    //else if ((value == 2) || (value==3) || (value==5) || (value==7)) {
-    //    return true;
-    //}
-    //
-    //else if ((value % 2 == 0) || (value%3==0) || (value%5==0) || (value%7==0)) {
-    //    return false;
-    //}
-    //
-    //else {
-    //    return true;
-    //}
-
 }
 
 //нахождение n-ого простого числа (в ряду)
 uint64_t nPrime(uint64_t n) {
 
-    const uint64_t size = 1000000;
-    uint64_t h = 0;
+    //const uint64_t size = 1000000;
+    //uint64_t h = 0;
+    //uint64_t value=0;
+    //for (uint64_t i = 2; i < size; i++) {
+    //    if (checkPrime(i)) {
+    //        h++;
+    //        if (h == n) {
+    //            value = i;
+    //            break;
+    //        }
+    //    }
+    //}
 
-    //uint64_t arr[size];
-    uint64_t value=0;
-    for (uint64_t i = 2; i < size; i++) {
-        if (checkPrime(i)) {
-            if (++h == n) {
-                value = i;
-                break;
-            }
+    uint64_t i = 0;
+    while (n > 0) {
+        while (!checkPrime(i)) {
+            i++;
         }
-          //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))){
-          //  arr[h++] = i;
-        //}
+        n--;
     }
-
-    return value;
+    return i;
 }
 
 //нахождение ближайшего следующего простого числа
 uint64_t nextPrime(uint64_t value) {
 
-    const uint64_t size = 1000000;
-    uint64_t res=0;
+    //const uint64_t size = 1000000;
+    //uint64_t res=0;
     uint64_t i;
-    
-    for (i = value+1; i < size; i++) {
-        //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
-        //    res = (uint64_t)i;
-        //    break;
-        //}
-        //else {
-        //    continue;
-        //}
-        if (checkPrime(i)) {
-            res = i;
-            break;
-        }
+    //
+    //for (i = value+1; i < size; i++) {
+    //    if (checkPrime(i)) {
+    //        res = i;
+    //        break;
+    //    }
+    //}
+    i = value + 1;
+    while (!checkPrime(i)) {
+        i++;
     }
-    return res;
+
+    return i;
 }
 
 //сумма всех чисел до hbound (не включая его)
@@ -83,10 +67,6 @@ uint64_t sumPrime(uint64_t hbound) {
     uint64_t summ = 0;
 
     for (int i = 2; i < hbound; i++) {
-        
-        //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
-        //    summ += (uint64_t)i;
-        //}
         if (checkPrime(i)) {
             summ += i;
         }
