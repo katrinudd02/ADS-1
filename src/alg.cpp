f4d5b4aa4cbcfdd6b87b10ec15410bab38d8f938
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-02-27 23:15:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,8 @@ using namespace std;
 //проверка числа на простоту
 bool checkPrime(uint64_t value) {
     bool flag = false;
-    if (((value % 2 != 0) && (value % 3 != 0) && (value % 5 != 0) && (value % 7 != 0)) || (value == 2) || (value == 3) || (value == 5) || (value == 7)) {
+    if (((value % 2 != 0) && (value % 3 != 0) && (value % 5 != 0) && (value % 7 != 0)) \
+      || (value == 2) || (value == 3) || (value == 5) || (value == 7)) {
         flag = true;
     }
     return flag;
@@ -27,20 +28,12 @@ uint64_t nPrime(uint64_t n) {
 
 //нахождение ближайшего следующего простого числа
 uint64_t nextPrime(uint64_t value) {
-    //const uint64_t size = 1000000;
-    //uint64_t res=0;
     uint64_t i;
-    //
-    //for (i = value+1; i < size; i++) {
-    //    if (checkPrime(i)) {
-    //        res = i;
-    //        break;
-    //    }
-    //}
     i = value + 1;
     while (!checkPrime(i)) {
         i++;
     }
+
     return i;
 }
 
