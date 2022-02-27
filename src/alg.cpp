670b30a50a5332c2e32d632aadf58566ec77a2f4
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-02-27 22:23:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,53 +7,71 @@ using namespace std;
 //проверка числа на простоту
 bool checkPrime(uint64_t value) {
     
-    if (value == 1) {
-        return false;
+    bool flag = false;
+    if (((value % 2 != 0) && (value % 3 != 0) && (value % 5 != 0) && (value % 7 != 0)) || (value == 2) || (value == 3) || (value == 5) || (value == 7)) {
+        flag = true;
     }
+    return flag;
 
-    else if ((value == 2) || (value==3) || (value==5) || (value==7)) {
-        return true;
-    }
-
-    else if ((value % 2 == 0) || (value%3==0) || (value%5==0) || (value%7==0)) {
-        return false;
-    }
-
-    else {
-        return true;
-    }
+    //if (value == 1) {
+    //    return false;
+   // }
+    //
+    //else if ((value == 2) || (value==3) || (value==5) || (value==7)) {
+    //    return true;
+    //}
+    //
+    //else if ((value % 2 == 0) || (value%3==0) || (value%5==0) || (value%7==0)) {
+    //    return false;
+    //}
+    //
+    //else {
+    //    return true;
+    //}
 
 }
 
 //нахождение n-ого простого числа (в ряду)
 uint64_t nPrime(uint64_t n) {
 
-    const int size = 1000000;
-    int h = 0;
+    const uint64_t size = 1000000;
+    uint64_t h = 0;
 
-    uint64_t arr[size];
-    for (int i = 0; i < size; i++) {
-        if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))){
-            arr[h++] = i;
+    //uint64_t arr[size];
+    uint64_t value=0;
+    for (uint64_t i = 2; i < size; i++) {
+        if (checkPrime(i)) {
+            if (++h == n) {
+                value = i;
+                break;
+            }
         }
+          //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))){
+          //  arr[h++] = i;
+        //}
     }
 
-    return arr[n - 1];
+    return value;
 }
 
 //нахождение ближайшего следующего простого числа
 uint64_t nextPrime(uint64_t value) {
 
-    int size = 1000000;
-    uint64_t res;
+    const uint64_t size = 1000000;
+    uint64_t res=0;
+    uint64_t i;
     
-    for (int i = value+1; i < size; i++) {
-        if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
-            res = (uint64_t)i;
+    for (i = value+1; i < size; i++) {
+        //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
+        //    res = (uint64_t)i;
+        //    break;
+        //}
+        //else {
+        //    continue;
+        //}
+        if (checkPrime(i)) {
+            res = i;
             break;
-        }
-        else {
-            continue;
         }
     }
     return res;
@@ -64,9 +82,13 @@ uint64_t sumPrime(uint64_t hbound) {
 
     uint64_t summ = 0;
 
-    for (int i = 0; i < hbound; i++) {
-        if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
-            summ += (uint64_t)i;
+    for (int i = 2; i < hbound; i++) {
+        
+        //if (((i % 2 != 0) && (i % 3 != 0) && (i % 5 != 0) && (i % 7 != 0)) || ((i == 2) || (i == 3) || (i == 5) || (i == 7))) {
+        //    summ += (uint64_t)i;
+        //}
+        if (checkPrime(i)) {
+            summ += i;
         }
     }
 
